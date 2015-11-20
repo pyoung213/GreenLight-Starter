20fdbc1b7ab583855c8e86b7396aa86f976537ef
Patrick Young <patrick.young@smartsocialmedia.com>
2015-11-19 23:57:07 -0600

    Renamed pages to views.  Fixed image path issue.

--- src/web/app/modals/forgotPasswordModal/forgotPasswordModal.js
+++ src/web/app/modals/forgotPasswordModal/forgotPasswordModal.js
@@ -1,61 +1,65 @@
-angular
-    .module('app.web')
-    .service('ForgotPasswordModal', ForgotPasswordModal)
-    .controller('ForgotPasswordModalController', ForgotPasswordModalController);
+(function() {
+    'use strict'
 
-function ForgotPasswordModal($mdDialog) {
-    var vm = {
-        open: open
-    };
+    angular
+        .module('app.web')
+        .service('ForgotPasswordModal', ForgotPasswordModal)
+        .controller('ForgotPasswordModalController', ForgotPasswordModalController);
 
-    return vm;
+    function ForgotPasswordModal($mdDialog) {
+        var vm = {
+            open: open
+        };
 
-    function open() {
-        return $mdDialog.show({
-            controller: 'ForgotPasswordModalController as vm',
-            templateUrl: 'forgotPasswordModal.html',
-            parent: angular.element(document.body),
-            targetEvent: event,
-            clickOutsideToClose: true
-        });
+        return vm;
+
+        function open() {
+            return $mdDialog.show({
+                controller: 'ForgotPasswordModalController as vm',
+                templateUrl: 'forgotPasswordModal.html',
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose: true
+            });
+        }
     }
-}
 
-function ForgotPasswordModalController($mdDialog, Auth, Logger, CurrentUser, glToast) {
-    Logger = Logger.specialize('Forgot Password Modal');
+    function ForgotPasswordModalController($mdDialog, Auth, Logger, CurrentUser, glToast) {
+        Logger = Logger.specialize('Forgot Password Modal');
 
-    var vm = {
-        submit: submit,
-        cancel: cancel
-    };
+        var vm = {
+            submit: submit,
+            cancel: cancel
+        };
 
-    return vm;
+        return vm;
 
-    function submit(form) {
+        function submit(form) {
 
-        if (form.$invalid) {
-            return;
+            if (form.$invalid) {
+                return;
+            }
+
+            Auth.forgotPassword(vm.email)
+                .then(function() {
+                    $mdDialog.hide();
+                })
+                .catch(function(err) {
+                    if (err.status === 404) {
+                        glToast.simple('FORGOT_PASSWORD.NO_ACCOUNT');
+                        return;
+                    }
+                    glToast.simple('FORGOT_PASSWORD.UNKNOWN_ERROR');
+
+                    Logger.error("An error occurred attempting to change the password", {
+                        email: CurrentUser.email,
+                        err: err
+                    });
+                });
         }
 
-        Auth.forgotPassword(vm.email)
-            .then(function() {
-                $mdDialog.hide();
-            })
-            .catch(function(err) {
-                if (err.status === 404) {
-                    glToast.simple('FORGOT_PASSWORD.NO_ACCOUNT');
-                    return;
-                }
-                glToast.simple('FORGOT_PASSWORD.UNKNOWN_ERROR');
-
-                Logger.error("An error occurred attempting to change the password", {
-                    email: CurrentUser.email,
-                    err: err
-                });
-            });
+        function cancel() {
+            $mdDialog.cancel();
+        }
     }
-
-    function cancel() {
-        $mdDialog.cancel();
-    }
-}
+})();
